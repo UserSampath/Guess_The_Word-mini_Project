232a8ef5759c3ecd3293dccafba6e44cf9b14226
UserSampath <nalakasampathsmp@gmail.com>
2024-05-05 16:16:20 +0530

    HangManDrawing man is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import HangManDrawing from "./HangManDrawing";
+import HangManWord from "./HangManWord";
+import Keyboard from "./Keyboard";
 
-const words=["sample","wsfe","fwefwf","sdfsdfsf"]
+const words = ["sample", "wsfe", "fwefwf", "sdfsdfsf"];
 
 function App() {
-
   const [wordToGuss, setWordToGuss] = useState(() => {
-    return words[Math.floor(Math.random()*words.length)]
-  })
+    return words[Math.floor(Math.random() * words.length)];
+  });
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
   return (
-    <>
-     <h1>hi</h1>
-    </>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}>
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Loos win</div>
+      <div><HangManDrawing />
+        <HangManWord />
+        <Keyboard/>
+
+      </div>
+      </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/HangManWord.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HangManWord = () => {
+  return <div>HangManWord</div>;
+}
+
+export default HangManWord
